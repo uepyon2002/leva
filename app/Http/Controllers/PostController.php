85a5bed43ee22b574901fa2919dffa435a2918c2
uepyon2002 <anokeryo@gmail.com>
2024-04-18 21:01:56 +0900

    初期設定&基礎実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class Postcontroller extends Controller
 {
-    //
+ public function index (Post $post) 
+ {
+    return $post->get();
+}
+
 }
