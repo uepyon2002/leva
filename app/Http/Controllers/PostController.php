c15425cdf53823abdf226a02eff5ac81d8f1b771
uepyon2002 <anokeryo@gmail.com>
2024-04-24 16:57:15 +0900

    記事投稿作成画面

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
+    }
+
+
+    public function show(Post $post)
+    {
+       return view('posts.show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+    return view('posts.create');
+    }
+    
+}  
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class,'create']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
